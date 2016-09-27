1dabd0932e767eab9a5085e1c27f8c0a3a380ec9
Lucy Fox <lucyreneefox@gmail.com>
2016-09-27 12:28:59 -0600

    Add default quality of 0 to ideas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927160400) do
+ActiveRecord::Schema.define(version: 20160927182717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20160927160400) do
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "quality"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "quality",    default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
