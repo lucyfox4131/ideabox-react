ed392acd0d2b17afea30053f5aceeb7442cc59a9
Lucy Fox <lucyreneefox@gmail.com>
2016-09-27 12:32:38 -0600

    Add test of default idea quality of 0

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'defaults to quality of 0' do
+    idea = Idea.create(title: "Idea Title", body: "Idea Body")
+
+    expect(idea.quality).to eq(0)
+  end
+
 end
