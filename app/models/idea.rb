d6ce5b20754197248cd5f90187e7310a52f51779
Lucy Fox <lucyreneefox@gmail.com>
2016-09-27 12:36:48 -0600

    Add title presence validation to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,3 @@
 class Idea < ApplicationRecord
+  validates :title, presence: true
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
 
-  it 'defaults to quality of 0' do
-    idea = Idea.create(title: "Idea Title", body: "Idea Body")
-
-    expect(idea.quality).to eq(0)
+  context "validations" do
+    it { should validate_presence_of(:title)}
   end
 
+  context "default quality" do
+    it 'defaults to quality of 0' do
+      idea = Idea.create(title: "Idea Title", body: "Idea Body")
+      expect(idea.quality).to eq(0)
+    end
+  end
 end
