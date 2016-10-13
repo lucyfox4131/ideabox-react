f3d99ac9eeea8e11ba8202f92a2e62d894378905
Lucy Fox <lucyreneefox@gmail.com>
2016-10-13 16:32:18 -0600

    Attempting to load table of ideas onto page on load

--- app/assets/javascripts/components/_ideas.js.jsx
+++
@@ -1,9 +0,0 @@
-var Ideas = React.createClass({
-  render(){
-    return(
-      <div>
-        <h1>IdeaBox - React</h1>
-      </div>
-    )
-  }
-});
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,5 @@
 class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
-    render component: 'Ideas', props: { ideas: @ideas }
   end
 end
