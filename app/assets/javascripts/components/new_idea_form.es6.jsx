e60e3ba0aaddebf36b87ff49ea2732e790d69230
Lucy Fox <lucyreneefox@gmail.com>
2016-10-14 15:57:04 -0600

    Attempt to add new idea form

--- app/assets/javascripts/components/new_idea_form.es6.jsx
+++ app/assets/javascripts/components/new_idea_form.es6.jsx
@@ -1,10 +1,32 @@
 class NewIdeaForm extends React.Component {
+  constructor(){
+    super();
+    this.state = {title: '', body: ''}
+  }
+  createNewIdea(e){
+    e.preventDefault();
+    this.props.onCreateIdea(this.state);
+    this.clearForm();
+  }
+
+  clearForm(){
+    this.setSate({title: '', body: ''})
+  }
+
+  onFieldChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  setState(object){
+    
+  }
+
   render () {
     return(
-      <form className="new-idea-form">
-        Title: <input type="text" name="Title" className="new-idea-form-title"></input>
-        Body: <input type="text" name="Body" className="new-idea-form-body"></input>
-        <input type="Submit" value="Submit"></input>
+      <form onSubmit={this.createNewIdea}>
+        <input value={this.state.title} name="title" className="new-idea-form-title" placeholder="Title" onChange={this.onFieldChange} />
+        <textArea value={this.state.body} name="body" className="new-idea-form-body" placeholder="Body" onChange={this.onFieldChange} />
+        <input className="new-idea-form-submit" type="submit" value="Create New Idea"/>
       </form>
     )
   }
